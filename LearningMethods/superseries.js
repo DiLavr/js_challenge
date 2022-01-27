e819773a3f591c41db6bcda6dee2aa93ee5b477a
Dmitrii Lavruk <dmitry@lavruk.com>
2022-01-27 09:15:25 -0500

    how wins

--- LearningMethods/superseries.js
+++ LearningMethods/superseries.js
@@ -11,7 +11,7 @@
 const scores = [
     [3, 7], // Первая игра
     [4, 1], // Вторая игра
-    [4, 4],
+    [4, 4], // NaN
     [3, 5],
     [4, 5],
     [3, 2],
@@ -19,11 +19,36 @@ const scores = [
     [6, 5],
     ];
 
-const res ()
+// const getSuperSeriesWinner = scores => {
+//     const matchresults = [];
+//     for (const item of scores) {
+//         const newItem = Math.sign(scores[item][0] - scores[item][1]);
+//         {
+//         matchresults.push(newItem); }
+//     }
+//     return matchresults;
+// };
 
 
-    // const superseries = score => {
-        
+// const a = Number(scores[0][0]);
+// const b = Number(scores[0][1]);
+// console.log(Math.sign(scores[0][0] - scores[0][1]));
+// const c = Number(a-b);
 
+const getSuperSeriesWinner = (scores) => { // берем массив scores
+    let result = 0; // объявляем начало 
+    for (const score of scores) { // перебираем массив с нуля
+      result += Math.sign(score[0] - score[1]); //вот эта часть не понятна - почему score, а не [score]?
+    }
+  
+    if (result > 0) {
+      return 'canada';
+    }
+    if (result < 0) {
+      return 'ussr';
+    }
+  
+    return null; // если нет возвращаем null
+  };
 
-    // }
+console.log(getSuperSeriesWinner(scores));
