2de5100259aecae6cd9371d651fcec6bb2337121
Dmitrii Lavruk <dmitry@lavruk.com>
2022-06-09 22:56:43 -0400

    example of Object.hasOwn function

---
+++ LearningMethods/objects/hasObjExample.js
@@ -0,0 +1,15 @@
+const user = { name: 'Petr', age: 10 };
+
+ 
+
+if (Object.hasOwn(user, 'car')) {
+
+  console.log('User has a car');
+
+} else {
+
+  console.log("User doesn't have a car");
+
+}
+
+//the result of User doesn't have a car
