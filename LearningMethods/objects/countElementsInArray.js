771f49a2789c01b1e74724580b4ddb75ecb27df8
Dmitrii Lavruk <dmitry@lavruk.com>
2022-06-09 21:29:20 -0400

    adding Object.hasOwn

--- LearningMethods/objects/countElementsInArray.js
+++ LearningMethods/objects/countElementsInArray.js
@@ -19,7 +19,26 @@ const bag = [
     return result;
   };
   
+  
+  // лучше и правельней способ проверить существование свойств в объектах, использовать метод Object.hasOwn
+  const countFruits1 = (fruits) => {
+    const result = {};
+  
+    for (const name of fruits) {
+      // Проверка на существование
+      // The Object.hasOwn() static method returns true if the specified object has the indicated property as its own property. 
+      // If the property is inherited, or does not exist, the method returns false
+      if (Object.hasOwn(result, name)) {
+        result[name] += 1;
+      } else {
+        result[name] = 1;
+      }
+    }
+  
+    return result;
+  };
+  
 
   
 console.log('test');
-  console.log(countFruits(bag));
+  console.log(countFruits1(bag));
